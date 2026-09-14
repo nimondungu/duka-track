a002068583a72a25c7e2f0079e37742b79a697e9
nimondungu <maureenndungu51@gmail.com>
2026-09-14 15:54:56 +0300

    Ensure database initializes on Gunicorn startup

--- app.py
+++ app.py
@@ -1646,5 +1646,4 @@ if __name__ == "__main__":
     init_db()
     app.run(debug=True, host="0.0.0.0", port=5000)
 else:
-    # This runs automatically when Gunicorn starts the app on Render
     init_db()
